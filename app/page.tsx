b3f82660d872d3b352e63a0c8d69e248defe37ba
gibbs119 <gibbs119@gmail.com>
2026-02-15 11:56:16 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // app/page.tsx
 'use client';
 
-import GolfScoringApp from '@/components/GolfScoringApp';  // adjust path if folder is 'Lib' not 'lib'
+import GolfScoringApp from '@/components/GolfScoringApp';
 
 export default function Home() {
   return <GolfScoringApp />;
